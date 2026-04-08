067d50b11bac9c174d5aea695a307f3ec79e327c
YogeshSoni-Cr <yogeshkumarsoni2004@gmail.com>
2026-04-08 14:36:04 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'core-ranker-v1';
+
+// Add the core files you want to load instantly
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/dashboard.html',
+  '/tests.html',
+  '/login.html',
+  'https://cdn.tailwindcss.com'
+];
+
+// Install Event: Cache the files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch Event: Serve from cache if available, otherwise go to network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+            return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Activate Event: Clean up old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
